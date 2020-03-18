4afed97edff28ceeb78e00820e68e69bd1865431
khoironi <masrony37@gmail.com>
2020-03-19 01:31:37 +0700

    dbtn1

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -113,7 +113,7 @@
 									<p class="card-text"><?php echo $p->namajp; ?></p>
 									<a href="<?php echo base_url('welcome/product/') . $p->idp ?>" class="btn btn-primary">DOWNLOAD</a>
 									<button type="button" class="btn btn-outline-warning">DEMO</button>
-									<button type="button" class="btn btn-success">
+									<button type="button" class="btn bbtn-outline-success">
 										Terjual <span class="badge badge-light">9</span>
 									</button>
 								</div>
