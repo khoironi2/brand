7f689940d20b86de3eeb25f0e4fca6136e1a107e
khoironi <masrony37@gmail.com>
2020-03-18 14:19:28 +0700

    rotate rule5

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -101,10 +101,55 @@ class Welcome extends CI_Controller
 			);
 
 			$this->M_product->insert($datamember);
-			$this->send($email,  $namalengkap, $harga_p, $nama_p);
+			$this->sendMail($email,  $namalengkap, $harga_p, $nama_p);
 			echo "Mantappp";
 		}
 		redirect();
+	}
+	public function sendMail($email)
+	{
+		// $to =  $this->input->post('from');  // User email pass here
+		$subject = 'Welcome To CodingMantra';
+
+		$from = 'pass your email ID';              // Pass here your mail id
+
+		$emailContent = '<!DOCTYPE><html><head></head><body><table width="600px" style="border:1px solid #cccccc;margin: auto;border-spacing:0;"><tr><td style="background:#000000;padding-left:3%"><img src="http://codingmantra.co.in/assets/logo/logo.png" width="300px" vspace=10 /></td></tr>';
+		$emailContent .= '<tr><td style="height:20px"></td></tr>';
+
+
+
+
+		$emailContent .= '<tr><td style="height:20px"></td></tr>';
+		$emailContent .= "<tr><td style='background:#000000;color: #999999;padding: 2%;text-align: center;font-size: 13px;'><p style='margin-top:1px;'><a href='http://codingmantra.co.in/' target='_blank' style='text-decoration:none;color: #60d2ff;'>www.codingmantra.co.in</a></p></td></tr></table></body></html>";
+
+
+
+		$config['protocol']    = 'smtp';
+		$config['smtp_host']    = 'ssl://smtp.gmail.com';
+		$config['smtp_port']    = '465';
+		$config['smtp_timeout'] = '60';
+
+		$config['smtp_user']    = 'muhammadronias@gmail.com';    //Important
+		$config['smtp_pass']    = 'hjm0hsol3h';  //Important
+
+		$config['charset']    = 'utf-8';
+		$config['newline']    = "\r\n";
+		$config['mailtype'] = 'html'; // or html
+		$config['validation'] = TRUE; // bool whether to validate email or not 
+
+
+
+		$this->email->initialize($config);
+		$this->email->set_mailtype("html");
+		$this->email->from($from);
+		$this->email->to($email);
+		$this->email->subject($subject);
+		$this->email->message($emailContent);
+		$this->email->send();
+
+		$this->session->set_flashdata('msg', "Mail has been sent successfully");
+		$this->session->set_flashdata('msg_class', 'alert-success');
+		return redirect();
 	}
 
 
